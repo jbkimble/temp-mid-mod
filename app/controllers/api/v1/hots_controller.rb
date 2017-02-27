7bd8df829f6b8aa751a1b369080b92cdc1f3baf0
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 16:29:03 -0700

    test

--- app/controllers/api/v1/hots_controller.rb
+++ app/controllers/api/v1/hots_controller.rb
@@ -1,5 +1,5 @@
-class Api::V1::HotsController < ApplicationController
-  def create
-    binding.pry
-  end
-end
+# class Api::V1::HotsController < ApplicationController
+#   def create
+#     binding.pry
+#   end
+# end
