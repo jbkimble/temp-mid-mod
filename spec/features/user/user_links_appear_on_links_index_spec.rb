f3913cfa8075a184ccf625a2065f26ab71a8b77a
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 12:57:06 -0700

    Change test to have javascript

--- spec/features/user/user_links_appear_on_links_index_spec.rb
+++ spec/features/user/user_links_appear_on_links_index_spec.rb
@@ -1,24 +1,17 @@
 require 'rails_helper'
 
-describe "As a user" do
-  context "when i visit /" do
-    it "shows me all my links" do
-      user = User.create(email: "test@gmail.com", password: "123", password_confirmation: "123")
+RSpec.describe "can create links", :js => :true do
+  scenario "Create a new link" do
+    user = User.create(email: "test@gmail.com", password: "123", password_confirmation: "123")
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      # visit '/'
-      # fill_in "link-url", with: "http://www.google.com"
-      # fill_in "link-title", with: "Google"
-      #
-      # click_button "Add Link"
-      # visit '/'
-      #
-      # expect(page).to have_content("http://www.google.com")
-      # expect(page).to have_content("Google")
-    end
+      visit '/'
+      fill_in "link-url", with: "http://www.google.com"
+      fill_in "link-title", with: "Google"
 
-    it "does not show me others links" do
+      click_button "Add Link"
 
-    end
+      expect(page).to have_content("http://www.google.com")
+      expect(page).to have_content("Google")
   end
 end
