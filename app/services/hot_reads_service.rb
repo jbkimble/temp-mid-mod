b2630ef9edc8fd320f5fec74c63ca48ae440da68
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 16:55:31 -0700

    Add error logging

--- app/services/hot_reads_service.rb
+++ app/services/hot_reads_service.rb
@@ -1,5 +1,6 @@
 class HotReadsService
   def self.update_urls(url)
+    puts "test IT GOT HERE AND THEN FAILEDDDDDD"
     uri      = URI("https://polar-journey-33706.herokuapp.com/api/v1/urls/")
     # uri      = URI("http://localhost:8080/api/v1/urls/")
     res = Net::HTTP.post_form(uri, 'url' => url)
