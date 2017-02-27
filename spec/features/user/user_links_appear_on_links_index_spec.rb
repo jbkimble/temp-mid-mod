7538ce1b2b757338fd8b9ba5d591dbb2d8bf37e4
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 13:55:19 -0700

    Add test for edit happy and sad paths

--- spec/features/user/user_links_appear_on_links_index_spec.rb
+++ spec/features/user/user_links_appear_on_links_index_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "can create links", :js => :true do
-  scenario "Create a new link" do
-    user = User.create(email: "test@gmail.com", password: "123", password_confirmation: "123")
+RSpec.describe "as a user", :js => :true do
+  scenario "I see my links on the link index" do
+      user = User.create(email: "test3@gmail.com", password: "123", password_confirmation: "123")
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      user2 = User.create(email: "test2@gmail.com", password: "123", password_confirmation: "123")
+      Link.create(title: "This", url: "http://www.test.com", user_id: user2.id)
 
       visit '/'
       fill_in "link-url", with: "http://www.google.com"
@@ -11,7 +14,8 @@ RSpec.describe "can create links", :js => :true do
 
       click_button "Add Link"
 
+      expect(page).to_not have_content("http://www.test.com")
       expect(page).to have_content("http://www.google.com")
       expect(page).to have_content("Google")
-  end
+    end
 end
