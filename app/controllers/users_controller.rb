5f222e7b2aa64d0f75f42a8a103b114f731d302d
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 11:39:33 -0700

    Add password_confirmation to params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,16 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
+      flash[:success] = "Welcome, your account was successfully created."
       redirect_to :root
     else
+      error_type = user.errors.full_messages.to_sentence
+      flash[:failure] = "#{error_type}. Your account failed to be created"
       redirect_to :signup
     end
   end
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
