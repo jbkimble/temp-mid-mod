4f814a057eb8093665843eb8d3defd677b1c4976
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 13:54:37 -0700

    Add Update logic

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,4 +5,28 @@ class LinksController < ApplicationController
 
     @links = current_user.links unless current_user == nil
   end
+
+  def update
+    @link = Link.find(params["id"])
+    if URI.parse(link_params["url"]).host
+      @link.update(link_params)
+      @link.save
+      flash[:success] = "Link was successfully updated"
+      redirect_to '/'
+    else
+      flash[:failure] = "Link failed to update. URL must be valid and lead with 'http://' or 'https://'"
+      redirect_to '/'
+    end
+  end
+
+  def edit
+    @link = Link.find(params["id"])
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:title, :url)
+
+  end
 end
