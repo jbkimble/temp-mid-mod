0d8109d05a3791392a811e70df37db64b347d4f5
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 15:32:56 -0700

    Add production link to ajax request

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -5,11 +5,18 @@ $(document).ready(function(){
   $('#links-list').on('click', 'button.mark-read', function(){
     var $this = $(this);
     var linkId = $this.parents('.link').data('id');
-    
+    var link = this.parentElement.parentElement.getElementsByTagName("p")[1].innerText
+
     $.ajax({
       url: '/api/v1/links/' + linkId,
       method: 'PATCH',
       data: {read: true}
+    });
+
+    $.ajax({
+      url: 'https://polar-journey-33706.herokuapp.com/api/v1/urls/',
+      method: 'post',
+      data: {url: link}
     });
   })
 })
