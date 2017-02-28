5ed5f771d9de0e80196b72e66f120b112437b450
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 17:44:27 -0700

    try

--- app/services/hot_reads_service.rb
+++ app/services/hot_reads_service.rb
@@ -2,7 +2,7 @@ class HotReadsService
   def self.update_urls(url)
     uri      = URI("https://polar-journey-33706.herokuapp.com/api/v1/urls/")
     # uri      = URI("http://localhost:8080/api/v1/urls/")
-    potatoe = url
+    potatoe = url.to_json
     res = Net::HTTP.post_form(uri, 'url' => potatoe)
   end
 end
