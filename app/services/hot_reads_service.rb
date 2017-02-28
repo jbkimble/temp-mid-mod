1d1efde7fed0ff8d0a7d7da34e94b4b0130c85f0
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 18:51:28 -0700

    Give up on post request being successful

--- app/services/hot_reads_service.rb
+++ app/services/hot_reads_service.rb
@@ -1,11 +1,11 @@
 class HotReadsService
   def self.update_urls(url)
-    uri      = URI("https://polar-journey-33706.herokuapp.com/api/v1/urls/")
+    # uri      = URI("https://polar-journey-33706.herokuapp.com/api/v1/urls/")
     # uri      = URI("http://localhost:8080/api/v1/urls/")
-    res = Net::HTTP.post_form(uri, 'url' => url)
-    #
-    # req = Net::HTTP::Post.new(uri)
-    # req.set_form_data('url' => url)
+    # res = Net::HTTP.post_form(uri, {'url' => url})
+
+
+
 
   end
 end
