fea4276a1dab24f446dba3b08b5bb8d2bcd69c77
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 17:59:05 -0700

    This

--- app/services/hot_reads_service.rb
+++ app/services/hot_reads_service.rb
@@ -1,8 +1,15 @@
 class HotReadsService
   def self.update_urls(url)
-    uri      = URI("https://polar-journey-33706.herokuapp.com/api/v1/urls/")
-    # uri      = URI("http://localhost:8080/api/v1/urls/")
-    potatoe = url.to_json
-    res = Net::HTTP.post_form(uri, 'url' => potatoe)
+    # uri      = URI("https://polar-journey-33706.herokuapp.com/api/v1/urls/")
+    uri      = URI("http://localhost:8080/api/v1/urls/")
+    # potatoe = url.to_json
+    # res = Net::HTTP.post_form(uri, 'url' => potatoe)
+    # res.content_type = 'multipart/form-data'
+
+    # http = Net::HTTP.new(uri.host, uri.port)
+    # request = Net::HTTP::Post.new(uri)
+    # request.set_form_data({"url" => url})
+    # uri = URI('http://www.example.com/search.cgi')
+    res = Net::HTTP.post_form(uri, {'url' => url})
   end
 end
