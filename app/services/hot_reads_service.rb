ae2033ee6626cc4760126ccaea11498312d69595
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 17:10:52 -0700

    this

--- app/services/hot_reads_service.rb
+++ app/services/hot_reads_service.rb
@@ -1,9 +1,7 @@
 class HotReadsService
   def self.update_urls(url)
-    puts "test IT GOT HERE AND THEN FAILEDDDDDD"
     uri      = URI("https://polar-journey-33706.herokuapp.com/api/v1/urls/")
     # uri      = URI("http://localhost:8080/api/v1/urls/")
     res = Net::HTTP.post_form(uri, 'url' => url)
-    # Net::HTTP.post(uri)
   end
 end
