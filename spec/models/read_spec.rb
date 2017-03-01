0af6e371f6d0895c3016bf6e20ba88f429348c51
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-28 23:42:42 -0700

    Validation testing

--- spec/models/read_spec.rb
+++ spec/models/read_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Read, type: :model do
+  context 'valid attributes' do
+    it 'is valid with all attributes' do
+      link = Link.create(title: "panda", url: "http://www.pandabear.com", read: false)
+      read = Read.create(link_id: link.id)
+      expect(read).to be_valid
+    end
+  end
 
 end
